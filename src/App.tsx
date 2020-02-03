981c28c045ee36e4f9e5df6172ff978b8ba7fed4
patrickbrown <patricksbrown1992@gmail.com>
2020-02-03 00:16:20 -0500

    reset count

--- src/App.tsx
+++ src/App.tsx
@@ -23,22 +23,30 @@ interface CalcProps {
 }
 
 interface CalcState{
-  count: number
+  count: number,
+  numResets: number
 }
 export default class App extends React.Component <CalcProps, CalcState>{
 
   constructor(props: CalcProps) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      numResets: 0
      
     };
     this.addToCount = this.addToCount.bind(this);
+    this.resetCount = this.resetCount.bind(this);
   }
 
   addToCount(e: any){
     e.preventDefault();
     this.setState({ count: this.state.count + 1});
+  }
+
+  resetCount(e: any){
+    e.preventDefault();
+    this.setState({ count: 0,  numResets: this.state.numResets + 1});
   }
   
 
@@ -48,6 +56,9 @@ export default class App extends React.Component <CalcProps, CalcState>{
         <p>
           <br/>
           <button onClick= {this.addToCount}>{this.state.count}</button>
+          <button onClick = {this.resetCount}>Reset</button>
+          <br/>
+          <p>{this.state.numResets}</p>
         </p>
       </div>
     )

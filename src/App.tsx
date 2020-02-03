600ce029de225599252b14b78d1919db9ce6927d
patrickbrown <patricksbrown1992@gmail.com>
2020-02-03 00:04:44 -0500

    calculator class app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Props } from 'react';
+
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export const App = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+  
+//         <p>
+//           hey hi
+//         </p>
+        
+          
+        
+//       </header>
+//     </div>
+//   );
+// }
+
+interface CalcProps {
+
 }
 
-export default App;
+interface CalcState{
+  count?: number
+}
+export default class App extends React.Component <CalcProps, CalcState>{
+
+  constructor(props: CalcProps) {
+    super(props);
+    this.state = {
+      count: 0
+     
+    };
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <p>
+          hi. This is the app
+          <br/>
+          <button>{this.state.count}</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+
